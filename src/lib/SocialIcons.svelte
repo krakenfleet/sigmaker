24fb7a1a333fb26eef9fb0dff915f892e80f6a71
Adam Shand <adam@shand.net>
2022-11-20 14:03:11 +1300

    refactored social links into SocialLinks.svelte

---
+++ src/lib/SocialIcons.svelte
@@ -0,0 +1,42 @@
+<script>
+  import { imagesBaseUrl, fonts } from './Signature.svelte'
+  console.log({imagesBaseUrl, fonts})
+  export let links
+</script>
+
+
+<table
+  id="socialLinks"
+  style={`display: inline-block; ${fonts}`}
+  cellspacing="0"
+  cellpadding="0"
+>
+  <tbody>
+    <tr style="text-align: center">
+      {#each links as link}
+        <td>
+          <a
+            href={link.url}
+            style="
+              display: inline-block;
+              padding: 0px;
+            "
+            ><img
+              src={`${imagesBaseUrl}/${link.name}.png`}
+              alt=""
+              style="
+                max-width: 24px;
+                display: block;
+              "
+              height="24"
+          /></a>
+        </td>
+        <td width="5"><div></div></td>
+      {/each}
+    </tr>
+  </tbody>
+</table>
+
+<style>
+  /* Must use inline styles with email. */
+</style>
